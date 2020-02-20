b93dca11ae1359b624d440cff17af2fadce6fa39
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-20 14:53:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog
+  @@all = []
+
+  def self.all
+    @@all
+  end
+  def initialize
+    save(self)
+  end
+
+  def self.print_all
+    @@all.each {|n| puts "#{n}"}
+  end
+
+  def save(self)
+    @@all << self
+  end
+end
+    
